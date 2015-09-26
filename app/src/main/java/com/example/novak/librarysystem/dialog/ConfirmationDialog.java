0f29f2276d5fd49b789fa18508020b2de1d416e8
novakduc <novakduc@gmail.com>
2015-09-27 00:27:12 +0700

    1509202136 Novakduc

--- app/src/main/java/com/example/novak/librarysystem/dialog/ConfirmationDialog.java
+++ app/src/main/java/com/example/novak/librarysystem/dialog/ConfirmationDialog.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.novak.librarysystem.R;
 
@@ -17,8 +18,11 @@ public class ConfirmationDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.add_member_confirmation);
+
         LayoutInflater layoutInflater = getActivity().getLayoutInflater();
         View v = layoutInflater.inflate(R.layout.dialog_add_member_confirmation, null);
+
+        TextView nameTextView = (TextView) v.findViewById(R.id.dialog_add_member_confirmation_name);
 
         return super.onCreateDialog(savedInstanceState);
     }
