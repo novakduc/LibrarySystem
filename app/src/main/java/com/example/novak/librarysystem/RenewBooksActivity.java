5d2a1c354c5bda90712c1122a479a7afdd6eb0e2
novakduc <novakduc@gmail.com>
2015-11-23 19:21:56 +0700

    150918 n.thanh

--- app/src/main/java/com/example/novak/librarysystem/RenewBooksActivity.java
+++ app/src/main/java/com/example/novak/librarysystem/RenewBooksActivity.java
@@ -1,5 +1,7 @@
 package com.example.novak.librarysystem;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +11,13 @@ public class RenewBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_renew_book);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        if (fragmentManager.findFragmentById(R.id.renew_books_member_id_fragment_container) == null) {
+            transaction.add(R.id.renew_books_member_id_fragment_container, new RenewBooksFragment());
+            transaction.commit();
+        }
     }
 }
