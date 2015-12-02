433ea86147477a74d5fc4d45fc3464307e966ade
novakduc <novakduc@gmail.com>
2015-12-02 18:56:28 +0700

    150918 n.thanh

--- app/src/main/java/com/example/novak/librarysystem/RenewBooksMemberIdFragment.java
+++ app/src/main/java/com/example/novak/librarysystem/RenewBooksMemberIdFragment.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Model.Library;
+import Model.Member;
 
 
 /**
@@ -23,7 +29,36 @@ public class RenewBooksMemberIdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_renew_books_member_id, container, false);
 
-        return inflater.inflate(R.layout.fragment_renew_books_member_id, container, false);
+        Button searchButton = (Button) view.findViewById(R.id.renew_book_member_search_button);
+        final EditText memberNameEditText =
+                (EditText) view.findViewById(R.id.renew_book_member_name_text_view);
+        final EditText memberAddressEditText =
+                (EditText) view.findViewById(R.id.renew_book_member_address_text_view);
+        final EditText memberPhoneEditText =
+                (EditText) view.findViewById(R.id.renew_book_member_phone_text_view);
+        final EditText memberIdEditText =
+                (EditText) view.findViewById(R.id.renew_book_member_id_edit_text);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Library library = Library.getInstance(getActivity());
+                String s = memberIdEditText.getText().toString();
+                try {
+                    Long id = Long.parseLong(s);
+                    Member member = library.searchMember(id);
+                    if (member != null) {
+                        memberNameEditText.setText(member.getName());
+                        memberAddressEditText.setText(member.getAddress());
+                        memberPhoneEditText.setText(member.getPhone());
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getActivity(), R.string.renew_book_member_id_invalid, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return view;
     }
 }
